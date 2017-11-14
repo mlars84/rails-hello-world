fe2d2a4d0cf56117c6071b545955542ecab729d2
Matt Larson <matt.a.larson@gmail.com>
2017-11-14 11:57:45 -0600

    ability to add and view post working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
-  get 'blog/new'
+  resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
